2c814f7256efc5b9a11dc124ca880184e595d9a7
Catalin Valeriu <piece21@gmail.com>
2026-03-25 19:25:39 +0200

    Add process forking and sleep functionality
    Implement process forking with sleep in C.

---
+++ ex_2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void){
+        printf("sleeping\n");
+        int pid1 = fork();
+        if (pid1 == 0){ 
+                sleep(5);
+                int pid2 = fork();
+                if (pid2 == 0) { printf("sleep 1\n"); sleep(1);}
+                printf("pid2 %d\n", pid2);
+        }       
+        printf("pid1 %d\n", pid1);
+}
